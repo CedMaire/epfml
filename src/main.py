21fe7bb6a5f8a6cf794107c5606bad179d91859e
Antonio Morais <antonio.morais@epfl.ch>
2018-10-24 13:50:46 +0200

    Finie Task34

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import data_loader
+from implementations import *
 
 if __name__ == "__main__":
     yb, input_data, ids = data_loader.load_data(data_loader.DATA_PATH_TRAIN)
@@ -6,3 +7,8 @@ if __name__ == "__main__":
     print(yb, len(yb))
     print(input_data, len(input_data))
     print(ids, len(ids))
+    
+    w, loss = least_squares(yb, input_data)
+    print(w, loss)
+    w, loss = ridge_regression(yb, input_data, 0.037)
+    print(w, loss)
