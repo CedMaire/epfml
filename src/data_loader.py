e6deb7f4db758ae21e2149d244a4ccccdcf53f7f
BenjaminDeleze <benjamin.deleze@epfl.ch>
2018-10-25 16:31:44 +0200

    -1 to 0

--- src/data_loader.py
+++ src/data_loader.py
@@ -81,7 +81,7 @@ def load_data(data_path, sub_sample=False):
     input_data = x[:, 2:]
 
     yb = np.ones(len(y))
-    yb[np.where(y == "b")] = -1
+    yb[np.where(y == "b")] = 0
 
     if sub_sample:
         yb = yb[::50]
