646a7ea599d692b264df9c7fa253519cc2d05e06
BenjaminDeleze <benjamin.deleze@epfl.ch>
2018-10-26 12:38:27 +0200

    cross validation d'un model(pourcentage)

--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -1,4 +1,3 @@
-import datetime
 import numpy as np
 
 def sigmoid(t):
@@ -28,8 +27,8 @@ def logistic_regression_gradient_descent(y, tx, gamma, max_iter):
         w = w - gamma * grad
         losses.append(loss)
         ws.append(w)
-        if(iter%100 == 0):
-            print("Current iteration={i}, loss={l}".format(i=iter, l=loss[0][0]))
+#         if(iter%100 == 0):
+#             print("Current iteration={i}, loss={l}".format(i=iter, l=loss[0][0]))
     return ws[len(ws) - 1].T[0], losses[len(losses) - 1][0][0]
             
 def regularized_logistic_regression(y, tx, w, lambd):
