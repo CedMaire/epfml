e26b716fce800f2b7fc9673685181617df7fda42
Maire Cedric <mairecedric@netplus.ch>
2018-10-28 00:02:01 +0200

    test sub-sets

--- src/cross_validation.py
+++ src/cross_validation.py
@@ -1,9 +1,9 @@
 
 # coding: utf-8
 
-# In[22]:
+# In[1]:
 
-
+import time
 import numpy as np
 from data_loader import load_data, DATA_PATH_TEST, DATA_PATH_TRAIN, DATA_PATH_SAMPLE_SUBMISSION_TEST
 import logistic_regression
@@ -14,8 +14,8 @@ from label_predictor import predict_labels
 
 # In[2]:
 
-
-# y, tx, ids_train = load_data(DATA_PATH_TRAIN)
+#print("Loading Data...")
+#y, tx, ids_train = load_data(DATA_PATH_TRAIN)
 
 
 # In[3]:
@@ -30,10 +30,10 @@ def build_k_indices(y, k_fold, seed):
     return np.array(k_indices)
 
 
-# In[30]:
+# In[44]:
 
 
-def cross_validation(y, x, k_indices, k):
+def cross_validation(y, x, k_indices, k, gamma, alpha):
     # get k'th subgroup in test, others in train
     test_indice = k_indices[k]
     train_indice = k_indices[~(np.arange(k_indices.shape[0]) == k)]
@@ -43,7 +43,7 @@ def cross_validation(y, x, k_indices, k):
     x_test = x[test_indice]
     x_train = x[train_indice]
     
-    w, loss_train = logistic_regression.regularized_logistic_regression_gradient_descent(y_train, x_train, 0.1, 1000,0.3)
+    w, loss_train = logistic_regression.regularized_logistic_regression_gradient_descent(y_train, x_train, gamma, 1000, alpha)
     loss_test = logistic_regression.calculate_loss(y_test, x_test, w)
     y_pred = predict_labels(w, x_test)
     counter = 0
@@ -51,11 +51,12 @@ def cross_validation(y, x, k_indices, k):
     for x in range(y_test.shape[0]):
         if y_pred[x] == y_test[x]:
             counter += 1
-    print(100*counter/y_test.shape[0])
-    return loss_train, loss_test,w
+    percent = 100*counter/y_test.shape[0]
+#     print(percent)
+    return loss_train, loss_test, w, percent
 
 
-# In[9]:
+# In[5]:
 
 
 def build_poly(tx, degree):
@@ -67,7 +68,7 @@ def build_poly(tx, degree):
     return arr_out
 
 
-# In[10]:
+# In[6]:
 
 
 def build_poly_one_column(x, degree):
@@ -77,7 +78,7 @@ def build_poly_one_column(x, degree):
     return arr
 
 
-# In[31]:
+# In[22]:
 
 
 def cross_validation_demo():
@@ -99,3 +100,125 @@ def cross_validation_demo():
         loss_te_tmp.append(loss_te)
         print("the loss of the training set is: ", np.mean(loss_tr_tmp))
         print("the loss of the test set is: ", np.mean(loss_te_tmp))
+
+#cross_validation_demo()
+
+
+# In[45]:
+
+
+def cross_validation_search_param(tx, y, set_um):
+    seed = 12
+    k_fold = 4
+    # split data in k fold
+    k_indices = build_k_indices(y, k_fold, seed)
+    
+    #Best parameter
+    bestW = []
+    bestRatio = 0
+    bestG = 0
+    bestA = 0
+    bestD = 0
+
+    gamma_range = ""
+    alpha_range = ""
+    degree_range = ""
+    if set_um == 0:
+#        gamma_range = np.arange(0.4, 0.61, 0.01)
+#        alpha_range = np.arange(0.4, 0.61, 0.01)
+#        degree_range = np.arange(2, 3, 1)
+        gamma_range = [0.4]
+        alpha_range = [0.4]
+        degree_range = [2]
+    elif set_um == 1:
+#        gamma_range = np.arange(0, 0.21, 0.01)
+#        alpha_range = np.arange(0, 0.21, 0.01)
+#        degree_range = np.arange(2, 3, 1)
+        gamma_range = [0.08]
+        alpha_range = [0.0]
+        degree_range = [2]
+    elif set_um == 2:
+#        gamma_range = np.arange(0.4, 0.61, 0.01)
+#        alpha_range = np.arange(0, 0.21, 0.01)
+#        degree_range = np.arange(2, 3, 1)
+        gamma_range = [0.5]
+        alpha_range = [0.1]
+        degree_range = [2]
+    elif set_um == 3:
+#        gamma_range = np.arange(0.5, 0.71, 0.01)
+#        alpha_range = np.arange(0, 0.21, 0.01)
+#        degree_range = np.arange(2, 3, 1)
+        gamma_range = [0.6]
+        alpha_range = [0.1]
+        degree_range = [2]
+    elif set_um == 4:
+#        gamma_range = np.arange(0.1, 0.21, 0.01)
+#        alpha_range = np.arange(0.7, 0.81, 0.01)
+#        degree_range = np.arange(2, 3, 1)
+        gamma_range = [0.15]
+        alpha_range = [0.7]
+        degree_range = [2]
+    elif set_um == 5:
+#        gamma_range = np.arange(0, 0.21, 0.01)
+#        alpha_range = np.arange(0, 0.21, 0.01)
+#        degree_range = np.arange(2, 3, 1)
+        gamma_range = [0.09]
+        alpha_range = [0.0]
+        degree_range = [2]
+    elif set_um == 6:
+#        gamma_range = np.arange(0, 0.21, 0.01)
+#        alpha_range = np.arange(0.2, 0.41, 0.01)
+#        degree_range = np.arange(2, 3, 1)
+        gamma_range = [0.06]
+        alpha_range = [0.2]
+        degree_range = [2]
+    elif set_um == 7:
+#        gamma_range = np.arange(0, 1.1, 0.1)
+#        alpha_range = np.arange(0, 1.1, 0.1)
+#        degree_range = np.arange(2, 3, 1)
+        gamma_range = [0.8]
+        alpha_range = [0.30000000000000004]
+        degree_range = [2]
+
+    #Test of different degrees
+    count = 0
+    for d in degree_range:  
+        tx_train = build_poly(tx, d)
+        for a in alpha_range:
+            for g in gamma_range:
+                # define lists to store the ratio of true mapping
+                ratio = 0
+                ratios= []
+                for k in range(k_fold):
+                    _, _, w, ratio = cross_validation(y, tx_train, k_indices, k, g, a)
+                    ratios.append(ratio)
+                if np.mean(ratios) > bestRatio:
+                    bestW = w
+                    bestG = g
+                    bestA = a
+                    bestD = d
+                    bestRatio = np.mean(ratios)
+                count += 1
+#                 if(count%100 == 0):
+#                     print(count)
+#                 print("ratio:", np.mean(ratios))
+    print("bestRatio:", bestRatio)
+    return bestW, bestG, bestA, bestD
+
+#counter = 0
+#for y_test, tx_test, id_test in zip(y, tx, ids_train):
+##    if counter != 7:
+##        counter += 1
+##        continue
+#    print("Start set:", counter)
+#    start = time.time()
+#    bestW, bestG, bestA, bestD = cross_validation_search_param(tx_test, y_test, counter)
+#    end = time.time()
+#    print("Time:", end - start)
+#    print("Set ", counter)
+#    print("bestW ", list(bestW))
+#    print("bestG ", bestG)
+#    print("bestA ", bestA)
+#    print("bestD ", bestD)
+#    print("--------------------------------------------------------------------------------")
+#    counter += 1
